28700f258eddcef9ac71079daa585df6d45aa56f
Marko Smiljic <markosmiljic99@gmail.com>
2025-08-20 16:53:44 +0200

    fix: file init

--- src/blitjs.ts
+++ src/blitjs.ts
@@ -1,9 +1,55 @@
 export namespace BlitJS {
 
+    export type Color = {
+        r: number,
+        g: number,
+        b: number,
+        a: number
+    }
+
+    export class Rect {
+        private _x: number;
+        private _y: number;
+        private _w: number;
+        private _h: number;
+
+        constructor(pos: [number, number], size: [number, number]) {
+            this._x = pos[0];
+            this._y = pos[1];
+            this._w = size[0];
+            this._h = size[1];
+        }
+
+        get width() { return this._w; }
+        set width(value: number) { this._w = value; }
+
+        get height() { return this._h; }
+        set height(value: number) { this._h = value; }
+
+        get x() { return this._x; }
+        set x(value: number) { this._x = value; }
+
+        get y() { return this._y; }
+        set y(value: number) { this._y = value; }
+
+        get left() { return this._x; }
+        set left(value: number) { this._x = value; }
+
+        get right() { return this._x + this._w; }
+        set right(value: number) { this._x = value - this._w }
+
+        get top() { return this._y; }
+        set top(value: number) { this._y = value; }
+
+        get bottom() { return this._y + this._h; }
+        set bottom(value: number) { this._y = value - this._h }
+    }
+
     export class Surface {
         canvas: HTMLCanvasElement = document.createElement('canvas');
         ctx: CanvasRenderingContext2D = this.canvas.getContext('2d') as CanvasRenderingContext2D;
         size: [number, number];
+        image: HTMLImageElement | null = null;
 
         constructor(size: [number, number]) {
             this.size = size;
@@ -20,6 +66,52 @@ export namespace BlitJS {
 
         blit(surface: Surface, pos: [number, number]): void {
             this.ctx.drawImage(surface.canvas, pos[0], pos[1]);
+        }
+
+        copy(): Surface {
+            return new Surface(this.size);
+        }
+    }
+
+    export namespace image {
+
+        export const load = (filename: string): Promise<Surface> => {
+            return new Promise((res, rej) => {
+                const imageElement = new Image();
+                imageElement.src = filename;
+                imageElement.onload = () => {
+                    const surf = new Surface([imageElement.width, imageElement.height]);
+                    surf.ctx.imageSmoothingEnabled = false;
+                    surf.ctx.drawImage(imageElement, 0, 0);
+                    surf.image = imageElement;
+                    res(surf);
+                }
+                imageElement.onerror = (err) => rej(err);
+            });
+        }
+    }
+
+    export namespace transform {
+
+        export const scale = (surf: Surface, size: [number, number]): Surface => {
+            const scaled = new Surface(size);
+            scaled.ctx.imageSmoothingEnabled = false;
+            scaled.ctx.drawImage(surf.canvas, 0, 0, size[0], size[1]);
+            return scaled;
+        }
+
+        export const flip = (surf: Surface, flip: [boolean, boolean]): Surface => {
+            const w = surf.size[0];
+            const h = surf.size[1];
+
+            const flipped = new Surface([w, h]);
+            flipped.ctx.imageSmoothingEnabled = false;
+
+            flipped.ctx.translate(flip[0] ? w : 0, flip[1] ? h : 0);
+            flipped.ctx.scale(flip[0] ? -1 : 1, flip[1] ? -1 : 1);
+            flipped.ctx.drawImage(surf.canvas, 0, 0);
+
+            return flipped;
         }
     }
 
@@ -39,15 +131,13 @@ export namespace BlitJS {
 
             fill(color: string = "black") {
                 this.surface.fill(color);
-                this.update();
             }
 
             blit(surf: Surface, pos: [number, number]): void {
                 this.surface.blit(surf, pos);
-                this.update();
             }
 
-            private update(): void {
+            update(): void {
                 this.ctx.drawImage(this.surface.canvas, 0, 0);
             }
         }
@@ -61,6 +151,60 @@ export namespace BlitJS {
 
         export const getSurface = (): Surface | null => {
             return display ? display.surface : null;
+        }
+
+        export const getSize = (): [number, number] => {
+            return display ? [display.surface.size[0], display.surface.size[1]] : [0, 0];
+        }
+
+        export const getWidth = (): number => {
+            return display ? display.surface.size[0] : 0;
+        }
+
+        export const getHeight = (): number => {
+            return display ? display.surface.size[1] : 0;
+        }
+    }
+
+    export namespace time {
+
+        export class Clock {
+            private _last = performance.now();
+
+            private _rawDelta = 0;
+            private _delta = 0;
+
+            private _fps = 0;
+            private _smoothing = 0.9;
+
+            private _maxDeltaMs = 250;
+
+            tick(): number {
+                const now = performance.now();
+                this._rawDelta = now - this._last;
+
+                this._delta = Math.min(this._rawDelta, this._maxDeltaMs);
+
+                const instFps = 1000 / (this._delta || 1);
+                this._fps = this._fps
+                ? this._smoothing * this._fps + (1 - this._smoothing) * instFps
+                : instFps;
+
+                this._last = now;
+                return this._delta;
+            }
+
+            getTime(): number {
+                return this._delta;
+            }
+
+            getRawTime(): number {
+                return this._rawDelta;
+            }
+
+            getFPS(): number {
+                return this._fps;
+            }
         }
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,58 @@
 import { BlitJS } from './blitjs'; 
 
-const display = BlitJS.display.setMode([1280, 720]);
+const screen = BlitJS.display.setMode([1280, 720]);
+const clock = new BlitJS.time.Clock();
+const display = new BlitJS.Surface([BlitJS.display.getWidth() / 2, BlitJS.display.getHeight() / 2]);
 
-const update = () => {
-    display.fill();
+const blueBox = new BlitJS.Surface([32, 32]);
+blueBox.fill('blue');
+
+const playerSurf = await BlitJS.image.load("./images/player.png");
+
+const rect = new BlitJS.Rect([200, 200], [50, 50]);
+
+let x: number = 50;
+let movement = [false, false];
+let flip: boolean = false;
+
+const loop = () => {
+    clock.tick();
+
+    display.fill('green');
 
     for (const e of BlitJS.event.get()) {
         if (e.type == BlitJS.event.EventType.KEYDOWN) {
-            console.log(e.key);
+            if (e.key == "a")
+                movement[0] = true;
+            if (e.key == "d")
+                movement[1] = true;
         }
-        if (e.type == BlitJS.event.EventType.MOUSEDOWN) {
-            console.log(e.button);
+        if (e.type == BlitJS.event.EventType.KEYUP) {
+            if (e.key == "a")
+                movement[0] = false;
+            if (e.key == "d")
+                movement[1] = false;
         }
     }
 
-    requestAnimationFrame(update);
+    let frameMovement: number = (Number(movement[1]) - Number(movement[0]));
+    x += frameMovement * 3;
+
+    if (frameMovement < 0)
+        flip = true;    
+    if (frameMovement > 0)
+        flip = false;
+    
+    display.blit(blueBox, [16 * 8, 16 * 10]);
+    display.blit(BlitJS.transform.flip(playerSurf, [flip, false]), [x, 16 * 13]);
+    
+    screen.blit(BlitJS.transform.scale(display, BlitJS.display.getSize()), [0, 0]);
+    
+    //screen.blit(display, [0, 0]);
+
+    screen.update();
+
+    requestAnimationFrame(loop);
 }
 
-update();
+loop();
